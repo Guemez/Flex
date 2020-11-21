fed7aa4e72405f09db6f13e8b7b810d8366d2693
ebovio <e.bovio08@gmail.com>
2020-11-21 02:02:16 -0600

    feat: adds footer

--- bamburgh-react-ui-kit-reactstrap-free/src/layout-components/Footer/index.js
+++ bamburgh-react-ui-kit-reactstrap-free/src/layout-components/Footer/index.js
@@ -11,7 +11,12 @@ import {
   Nav,
   NavItem,
   NavLink,
-  Button
+  Button,
+  FormText,
+  Form,
+  Label,
+  FormGroup,
+  Input,
 } from 'reactstrap';
 
 class Footer extends Component {
@@ -23,10 +28,10 @@ class Footer extends Component {
             <div className="container pt-sm-0 pt-md-5">
               <div className="px-0 col-lg-6 col-md-8 col-sm-12 mx-auto text-center">
                 <h1 className="display-3 mb-4 text-white font-weight-bold">
-                  Stay up to date
+                  Stay in touch with us 
                 </h1>
                 <p className="font-size-lg text-white-50">
-                  Follow us on any of our social media accounts to find out when
+                  Follow us on any of our social media to find out when
                   we release new products or updates.
                 </p>
               </div>
@@ -35,7 +40,7 @@ class Footer extends Component {
                 <NavItem>
                   <NavLink
                     className="nav-link text-white-50"
-                    href="https:&#x2F;&#x2F;www.facebook.com&#x2F;UiFort"
+                    href="https://www.facebook.com/FlexCorporate/"
                     rel="nofollow"
                     target="_blank"
                     id="btnFacebookTooltip3457">
@@ -53,7 +58,7 @@ class Footer extends Component {
                 <NavItem>
                   <NavLink
                     className="nav-link text-white-50"
-                    href="https:&#x2F;&#x2F;twitter.com&#x2F;uifort1"
+                    href="https://twitter.com/flexintl"
                     rel="nofollow"
                     target="_blank"
                     id="btnTwitterTooltip3457">
@@ -71,7 +76,7 @@ class Footer extends Component {
                 <NavItem>
                   <NavLink
                     className="nav-link text-white-50"
-                    href="https:&#x2F;&#x2F;www.instagram.com&#x2F;uifort1"
+                    href="https://www.instagram.com/flexintl/"
                     rel="nofollow"
                     target="_blank"
                     id="btnInstagramTooltip3457">
@@ -89,55 +94,37 @@ class Footer extends Component {
                 <NavItem>
                   <NavLink
                     className="nav-link text-white-50"
-                    href="https:&#x2F;&#x2F;discord.gg&#x2F;mddFBQX"
+                    href="https://www.youtube.com/c/flexintl"
                     rel="nofollow"
                     target="_blank"
-                    id="btnDiscordTooltip3457">
+                    id="btnYoutubeTooltip3457">
                     <span className="btn-wrapper--icon">
                       <FontAwesomeIcon
-                        icon={['fab', 'discord']}
+                        icon={['fab', 'youtube']}
                         className="font-size-xxl"
                       />
                     </span>
                   </NavLink>
-                  <UncontrolledTooltip target="btnDiscordTooltip3457">
-                    Discord
+                  <UncontrolledTooltip target="btnYoutubeTooltip3457">
+                    Youtube
                   </UncontrolledTooltip>
                 </NavItem>
                 <NavItem>
                   <NavLink
                     className="nav-link text-white-50"
-                    href="https:&#x2F;&#x2F;dribbble.com&#x2F;UiFort"
+                    href="https://www.linkedin.com/company/flexintl/"
                     rel="nofollow"
                     target="_blank"
-                    id="btnDribbbleTooltip3457">
+                    id="btnLinkedInTooltip3457">
                     <span className="btn-wrapper--icon">
                       <FontAwesomeIcon
-                        icon={['fab', 'dribbble']}
+                        icon={['fab', 'linkedin']}
                         className="font-size-xxl"
                       />
                     </span>
                   </NavLink>
-                  <UncontrolledTooltip target="btnDribbbleTooltip3457">
-                    Dribbble
-                  </UncontrolledTooltip>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className="nav-link text-white-50"
-                    href="https:&#x2F;&#x2F;github.com&#x2F;uifort"
-                    rel="nofollow"
-                    target="_blank"
-                    id="btnGithubTooltip3457">
-                    <span className="btn-wrapper--icon">
-                      <FontAwesomeIcon
-                        icon={['fab', 'github']}
-                        className="font-size-xxl"
-                      />
-                    </span>
-                  </NavLink>
-                  <UncontrolledTooltip target="btnGithubTooltip3457">
-                    Github
+                  <UncontrolledTooltip target="btnLinkedInTooltip3457">
+                    LinkedIn
                   </UncontrolledTooltip>
                 </NavItem>
               </Nav>
@@ -147,91 +134,46 @@ class Footer extends Component {
                   <CardBody>
                     <div className="text-black">
                       <h1 className="display-4 mb-3 font-weight-bold">
-                        Newsletter updates
+                        Any problem with the dashboard?
                       </h1>
                       <p className="font-size-md mb-4 text-black-50">
-                        Subscribe to our newsletter to be the first to find out
-                        when we offer promotions or discounts for our products.
+                        If you find any issue or problem while working with the dashboard, 
+                        send us a message so we can improve your experience. 
                       </p>
-                      <div id="mc_embed_signup">
-                        <form
-                          action="https://gmail.us3.list-manage.com/subscribe/post?u=3ebc33a3d37f2278158d74721&amp;id=a122e673aa"
-                          className="validate"
-                          id="mc-embedded-subscribe-form"
-                          method="post"
-                          name="mc-embedded-subscribe-form"
-                          noValidate
-                          target="_blank">
-                          <Row>
-                            <Col md="8" className="pr-md-0">
-                              <div id="mc_embed_signup_scroll">
-                                <div className="mc-field-group">
-                                  <input
-                                    className="form-control required email"
-                                    id="mce-EMAIL"
-                                    name="EMAIL"
-                                    placeholder="Your email address..."
-                                    type="email"
-                                  />
-                                </div>
-                                <div className="clear" id="mce-responses">
-                                  <div
-                                    className="response"
-                                    id="mce-Errorresponse"
-                                    style={{ display: 'none' }}
-                                  />
-                                  <div
-                                    className="response"
-                                    id="mce-success-response"
-                                    style={{ display: 'none' }}
-                                  />
-                                </div>
-                                <div
-                                  aria-hidden="true"
-                                  style={{
-                                    position: 'absolute',
-                                    left: '-5000px'
-                                  }}>
-                                  <input
-                                    name="b_3ebc33a3d37f2278158d74721_a122e673aa"
-                                    tabIndex="-1"
-                                    type="text"
-                                  />
-                                </div>
-                              </div>
-                            </Col>
-                            <Col md="4" className="mt-3 mt-md-0">
-                              <Button
-                                block
-                                className="w-100"
-                                color="first"
-                                id="mc-embedded-subscribe"
-                                name="subscribe"
-                                type="submit">
-                                <span className="btn-wrapper--label">
-                                  Sign up
-                                </span>
-                              </Button>
-                            </Col>
-                          </Row>
-                        </form>
-                      </div>
-                      <small className="text-black-50 d-block pt-3">
-                        We promise not to spam your inbox. We also hate spam!
-                      </small>
+                      <Form>
+                        <FormGroup>
+                          <Label htmlFor="exampleEmail" align="left">Email</Label>
+                          <Input
+                            type="email"
+                            name="email"
+                            id="exampleEmail"
+                            placeholder="Your email address"
+                          />
+                        </FormGroup>
+                        <FormGroup>
+                          <Label htmlFor="exampleText">How can we help you?</Label>
+                          <Input type="textarea" name="text" id="exampleText" placeholder="Tell us a bit about the issue you're facing"/>
+                        </FormGroup>
+                        <Button
+                          block
+                          className="w-100"
+                          color="first"
+                          id="mc-embedded-subscribe"
+                          name="contact"
+                          type="submit">
+                          <span className="btn-wrapper--label">
+                            Contact
+                          </span>
+                        </Button>
+                      </Form>
                     </div>
                   </CardBody>
                 </Card>
               </div>
               <div className="mt-5">
                 <small className="text-center d-block text-white-50">
-                  Copyright &copy; 2020 -{' '}
-                  <a
-                    className="text-white"
-                    href="https://uifort.com"
-                    title="UiFort">
-                    UiFort.com
-                  </a>
+                  &copy; 2020 FLEX LTD. All rights reserved. - 
+                  Flextronics International, LTD. All rights reserved.
                 </small>
               </div>
             </div>
